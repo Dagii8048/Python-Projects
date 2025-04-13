fb05a088d242b5ca1b9c35f2a23e9490233f53d0
Dagi <minaledagmawi@gmail.com>
2025-04-13 23:58:13 +0300

    updated gueeNumbers.py

--- guessNumbers.py
+++ guessNumbers.py
@@ -3,4 +3,16 @@ import random
 def guess(x):
 	random_number = random.randint(1,20)
 	attempts = 0
-	
+	while True:
+		try:
+			guess = int(input(f"Guess a number between 1 and {x}: "))
+			attempts += 1
+			if guess < random_number:
+				print("Sorry, guess again. Too low.")
+			elif guess > random_number:
+				print("Sorry, guess again. Too high.")
+			else:
+				print(f"Yay, congrats. You have guessed the number {random_number} correctly in {attempts} attempts!")
+				break
+		except ValueError:
+			print("Sorry, invalid input. Please enter an integer.")
